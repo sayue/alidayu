4e37c4c6d2b1bd2e35342e9088ca7a05045e4813
sayue <likehuyi92@gmail.com>
2017-11-07 16:54:32 +0800

    添加globalconfig全局配置,继承原SDK全局常量定义

---
+++ tests/test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Test
+ * @authors sayue (likehuyi92@gmail.com)
+ * @date    2017-11-07 16:50:52
+ * @version $Id$
+ */
+require dirname(__DIR__).'/vendor/autoload.php';
+
+var_dump(TOP_SDK_WORK_DIR);
+exit;
